fe88fefc96098eed365d02b7be2d840b0472f35a
Vittoria Zago <vittoriazago1@hotmail.com>
2019-06-02 22:49:30 -0300

    Atualizando testes

--- HealthCheck.API.Test/UnitTest1.cs
+++ HealthCheck.API.Test/UnitTest1.cs
@@ -1,21 +1,22 @@
 ﻿using HealthCheck.API.Controllers;
 using HealthCheck.Helper;
+using HealthCheck.Helper.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
+using System.Web.Http.Results;
 
 namespace HealthCheck.API.Test
 {
     [TestClass]
     public class UnitTest1
     {
-        //private readonly ApiServer _server;
-        //private readonly HttpClientWrapper _client;
-
         [TestMethod]
         public void TestMethod1()
         {
-            var erros = HealthCheckHelper.TestaControllersInjection("HealthCheck.API");
-            Assert.IsFalse(erros.Any());
+            var controller = new HealthCheckController();
+            var result = controller.Get() as OkNegotiatedContentResult<HealthCheckResponse>;
+
+            Assert.IsFalse(result.Content.errors.Any());
         }
     }
 }
